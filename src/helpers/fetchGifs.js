e8e1a34acb14c6be65d9d83d143b20f59240d6cd
Alejandro <alejandro@urspos.com>
2023-02-26 15:30:43 -0500

    first commit

---
+++ src/helpers/fetchGifs.js
@@ -0,0 +1,20 @@
+export const fetchGifs = async (category) => {
+
+    const url = `http://api.giphy.com/v1/gifs/search?api_key=nulvUrsZKh2LS1JrWLuOtZ7k2gYfZPgb&q=${category}&limit=5`
+    const resp = await fetch (url)
+
+    const {data} = await resp.json()
+
+    const gifs = data.map( ({id, title, images, url, username, user, rating, type}) => ({
+        id: id,
+        title: title,
+        thumbnail: images?.downsized_medium?.url,
+        url: url,
+        user: username,
+        userThumbnail: user?.avatar_url,
+        userUrl: user?.profile_url,
+        rating: rating,
+        type: type,
+    }))
+    return gifs
+}
